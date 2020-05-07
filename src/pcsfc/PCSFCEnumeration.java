227e7d210279fdc029acd0d86d73a1abc1ebe71b
TheophileStauder <44049966+TheophileStauder@users.noreply.github.com>
2020-05-07 10:46:47 +0200

    Add files via upload
    Début transformation de PCSFC vers PCLC
    Nouvelle commande dans la console "printFormulaPCLC" pour afficher les formules dans le formalise PCLC

---
+++ src/pcsfc/PCSFCEnumeration.java
@@ -0,0 +1,55 @@
+package fr.loria.k.revisor.engine.revisorPCSFC.pcsfc;
+
+import java.util.ArrayList;
+
+import java.util.Collection;
+
+import fr.loria.k.revisor.engine.revisorPCSFC.RevisorPCSFC;
+import fr.loria.k.revisor.engine.revisorPCSFC.pclc.*;
+public class PCSFCEnumeration extends PCSFCFormula {
+
+	private String name;
+	private String modality;
+	private ArrayList<String> unusedModalities;
+	
+	public PCSFCEnumeration(String n, String mod, Collection<String> um) {
+		this.name = n;
+		this.modality = mod;
+		this.unusedModalities = new ArrayList<>(um);
+		this.unusedModalities.remove(this.modality);
+	}
+
+	@Override
+	public String toString(boolean latex) {
+		if (latex) {
+			return " \\:" + RevisorPCSFC.formatNameToLatex(this.name) + "\\: = \\:" + this.modality;
+		}
+		return " " + this.name + " = " + this.modality;
+	}
+
+	@Override
+	public boolean canRevise() {
+		return false;
+	}
+
+	@Override
+	public PCSFCFormula toPCLC() {
+		/*PCSFCFormula pclc;
+		String modalityWithoutQuotes = this.modality.replace("\"", "");
+		pclc = new PCSFCBoolean("boolean_encoding_" + modalityWithoutQuotes).toPCLC();
+		for (String s: this.unusedModalities) {
+			modalityWithoutQuotes = s.replace("\"", "");
+			pclc = new PCSFCAnd(pclc, new PCSFCNot(new PCSFCBoolean("boolean_encoding_" + modalityWithoutQuotes).toPCLC()));
+		}
+		return pclc;*/
+		// Théophile : for (chaque enum =>new Booleen )
+		//retourne tout les booleen.PCLC
+		return null;
+	}
+	
+	@Override
+	public PCSFCFormula toPCSFC() {
+		return new PCSFCEnumeration(this.name, this.modality, this.unusedModalities);
+	}
+
+}
---
+++ src/pcsfc/PCSFCTautology.java
@@ -0,0 +1,37 @@
+package fr.loria.k.revisor.engine.revisorPCSFC.pcsfc;
+
+
+import fr.loria.k.revisor.engine.revisorPCSFC.pclc.*;
+
+
+public class PCSFCTautology extends PCSFCFormula {
+
+	public static final String LATEX_TAUTOLOGY = "{\\top}";
+	public static final String TAUTOLOGY = "tautology";
+	
+	public String toString(boolean latex) {
+		StringBuilder str = new StringBuilder();
+		if (latex) {
+			str.append(LATEX_TAUTOLOGY);
+		}
+		else {
+			str.append(TAUTOLOGY);
+		}
+		return str.toString();
+	}
+
+	@Override
+	public PCSFCFormula toPCLC() {
+		return new PCSFCTautology();
+	}
+	
+	@Override
+	public PCSFCFormula toPCSFC() {
+		return new PCSFCTautology();
+	}
+
+	@Override
+	public boolean canRevise() {
+		return true;
+	}
+}
